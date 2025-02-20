856f21fdb5b38ca9f37b0d5505ca8d2e486bd77f
edoartworks <edoardospinozzi@gmail.com>
2025-02-20 17:42:27 +0800

    Layout polish + started adding extra help screen

--- Game/screen_help.gd
+++ Game/screen_help.gd
@@ -1,6 +1,21 @@
 extends CanvasLayer
 
 
+@export var screen_more_path :NodePath
+var SCREEN_MORE: Node = null
+
+
+func _ready() -> void:
+	SCREEN_MORE = get_node(screen_more_path)
+
 
 func _on_btn_exit_pressed() -> void:
 	SignalBus.help_screen_exit_pressed.emit()
+
+
+func _on_btn_more_pressed() -> void:
+	SCREEN_MORE.visible = true
+
+
+func _on_btn_more_exit_pressed() -> void:
+	SCREEN_MORE.visible = false
